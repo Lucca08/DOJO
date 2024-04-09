902994da1e931d38979083bfe6a92cd29a51631d
pedro felipe <pedrofelipinho@gmail.com>
2024-04-09 16:16:50 -0300

    Adicionando transferirValor na interface

--- DOJO/src/main/java/com/example/DOJO/interfaceOperacoes/Operacoes.java
+++ DOJO/src/main/java/com/example/DOJO/interfaceOperacoes/Operacoes.java
@@ -5,5 +5,18 @@ public interface Operacoes {
     void saque(double valor);
     double consultarSaldo();
 
-    
+    default void transferirValorParaOutraConta(Conta contaDestino, double valor) {
+        if (valor <= 0) {
+            System.out.println("Valor inválido");
+        }
+
+        if (this.consultarSaldo < valor) {
+            System.out.println("Saldo insuficiente");
+        } else {
+            this.saque(valor);
+            contaDestino.deposito(valor);   
+        }
+    }
+
+        
 }
