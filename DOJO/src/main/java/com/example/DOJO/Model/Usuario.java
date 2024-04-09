b477ec9a69d12002155270f26706e4f9ac0a4b65
pedro felipe <pedrofelipinho@gmail.com>
2024-04-09 16:09:32 -0300

    Alterando o usuario

--- DOJO/src/main/java/com/example/DOJO/Model/Usuario.java
+++ DOJO/src/main/java/com/example/DOJO/Model/Usuario.java
@@ -1,44 +1,83 @@
 package com.example.DOJO.Model;
-
-import com.example.DOJO.Contas.ContaCorrente;
-
+ 
+import java.util.ArrayList;
+import java.util.List;
+ 
 public class Usuario {
     private String nome;
     private Long idDoUSuario;
-    private boolean temCC;
-    private boolean temCP;
-    private boolean temCI;
-    private Conta contaCorrente;
-    
-    
-    public Usuario(String nome, Long idDoUSuario, boolean temCC, boolean temCP, boolean temCI) {
+    private Long idDaConta;
+    private List<Conta> contas;
+   
+   
+    public Usuario(String nome, Long idDoUSuario, Long idDaConta) {
         this.nome = nome;
         this.idDoUSuario = idDoUSuario;
-        this.temCC = false;
-        this.temCP = false;
-        this.temCI = false;
-        this.contaCorrente = new ContaCorrente(idDoUSuario, idDoUSuario, 0);
+        this.idDaConta = idDaConta;
+        this.contas = new ArrayList<>();
     }
-
+ 
+    public void adicionarConta(Conta conta) {
+        if (contas.size() < 3) {
+            contas.add(conta);
+        } else {
+            System.out.println("O úsuario já tem o número máximo de contas");
+        }
+    }
+ 
+    public void deletarConta(Conta conta) {
+        if (contas.contains(conta)) {
+            contas.remove(conta);
+        } else {
+            System.out.println("Conta não encontrada");
+        }
+    }
+ 
+    public double consultarSaldoTotaldasContas() {
+        double saldoTotal = 0;
+        for (Conta conta : contas){
+            saldoTotal += conta.consultarSaldo();
+        }
+        return saldoTotal;
+    }
+ 
     public String getNome() {
         return nome;
     }
-
+ 
     public Long getIdDoUSuario() {
         return idDoUSuario;
     }
-
-    public boolean getTemCC() {
-        return temCC;
+ 
+    public Long getIdDaConta() {
+        return idDaConta;
     }
 
-    public boolean getTemCP() {
-        return temCP;
+    public void fecharConta(Conta conta){
+        if (conta.ContaCorrente){
+            System.out.println("Conta corrente não pode ser fechada");
+        } 
+        if (contas.contains(conta)) {
+            double saldoConta = conta.consultarSaldo();
+            if (saldoConta > 0) {
+                for (Conta c : contas) {
+                    if (c.ContaCorrente){
+                        c.depositar(saldoConta);
+                        System.out.println("Saldo da conta transferido para a conta corrente");
+                        contas.remove(conta);
+                        System.out.println("Conta foi fechada");
+                        return;
+                    }
+                }
+                System.out.println("Não tem conta corrente para usuário"); 
+                else {
+                    contas.remove(conta);
+                    System.out.println("Conta fechada");
+                }
+                else {
+                    System.out.println("Conta não foi encontrada");
+                }
+            }
+        }
     }
-
-    public boolean getTemCI() {
-        return temCI;
-    }
-
-
 }
