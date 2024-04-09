41eac09a4d0f7c9ef7d73c33ec4fe7688a002266
pedro felipe <pedrofelipinho@gmail.com>
2024-04-09 16:59:12 -0300

    Adicionando a main no dojo

--- DOJO/src/main/java/com/example/DOJO/DojoApplication.java
+++ DOJO/src/main/java/com/example/DOJO/DojoApplication.java
@@ -7,7 +7,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DojoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DojoApplication.class, args);
+	SpringApplication.run(DojoApplication.class, args);
+	
+
+	Usuario usuario = new Usuario("João", 1);
+	
+	ContaCorrente contaCorrente = new ContaCorrente(1L, 1L, 0);
+	usuario.adicionarConta(contaCorrente);
+
+	contaCorrente.deposito(1000);
+
+	ContaInvestimento contaInvestimento = new ContaInvestimento(2L, 1L, 0);
+	usuario.adicionarConta(contaInvestimento);
+
+	contaInvestimento.deposito(2000);
+
+	contaInvestimento.transferirValorParaOutraConta(contaCorrente, contaInvestimento.consultarSaldo());
+
+	try {
+		ContaCorrente.sacar(2000);
+	} catch (IllegalArgumentException e) {
+		System.out.println(e.getMessage("Não foi possivel sacar"));
 	}
 
+	System.out.println("Saldo da conta corrente: " + contaCorrente.consultarSaldo());
+    	
+	}
 }
